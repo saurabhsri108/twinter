003f4a5ff45fd3ba020aab6a533e9fd72df37ca2
Saurabh Srivastava <vasudeveloper001@gmail.com>
2022-02-07 01:16:01 +0530

    Added Moralis Authentication using Metamask Wallet

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,7 @@
 export const environment = {
-  production: true
+  production: true,
+  moralis: {
+    appId: 'dHZGmYxMbvUI5Da4O8sw3aC46oJP2ewUqF9Ld80t',
+    serverUrl: 'https://wrjzfuhxw9cc.usemoralis.com:2053/server',
+  },
 };
