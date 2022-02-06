e168a2057ebd1ca581eafa635bf1fd51ad465511
Saurabh Srivastava <vasudeveloper001@gmail.com>
2022-02-07 00:25:43 +0530

    Removed keys of Moralis

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,8 +6,8 @@ export const environment = {
   production: false,
   env: 'dev',
   moralis: {
-    appId: 'dHZGmYxMbvUI5Da4O8sw3aC46oJP2ewUqF9Ld80t',
-    serverUrl: 'https://wrjzfuhxw9cc.usemoralis.com:2053/server',
+    appId: '',
+    serverUrl: '',
   },
 };
 
