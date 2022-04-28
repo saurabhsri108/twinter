ba8a4f793d049d48a0527d559535af7ac0ebcba1
Saurabh Srivastava <saurabhsri@geekyants.com>
2022-04-28 16:49:02 +0530

    Deleted angular frontend

--- src/environments/environment.ts
+++
@@ -1,7 +0,0 @@
-export const environment = {
-  production: false,
-  moralis: {
-    appId: 'dHZGmYxMbvUI5Da4O8sw3aC46oJP2ewUqF9Ld80t',
-    serverUrl: 'https://wrjzfuhxw9cc.usemoralis.com:2053/server',
-  },
-};
